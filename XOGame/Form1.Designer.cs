e1171d13e59d96cb9b7af4c7df5ae350069660b5
Samuel Faris <samuelfaris@gmail.com>
2017-10-11 17:01:06 -0700

    First commit

---
+++ XOGame/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace XOGame
+{
+    partial class XOForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._00Button = new System.Windows.Forms.Button();
+            this._01Button = new System.Windows.Forms.Button();
+            this._02Button = new System.Windows.Forms.Button();
+            this._10Button = new System.Windows.Forms.Button();
+            this._11Button = new System.Windows.Forms.Button();
+            this._12Button = new System.Windows.Forms.Button();
+            this._20Button = new System.Windows.Forms.Button();
+            this._21Button = new System.Windows.Forms.Button();
+            this._22Button = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this._turnLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _00Button
+            // 
+            this._00Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._00Button.Location = new System.Drawing.Point(12, 12);
+            this._00Button.Name = "_00Button";
+            this._00Button.Size = new System.Drawing.Size(75, 75);
+            this._00Button.TabIndex = 0;
+            this._00Button.UseVisualStyleBackColor = true;
+            this._00Button.Click += new System.EventHandler(this._00Button_Click);
+            // 
+            // _01Button
+            // 
+            this._01Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._01Button.Location = new System.Drawing.Point(104, 12);
+            this._01Button.Name = "_01Button";
+            this._01Button.Size = new System.Drawing.Size(75, 75);
+            this._01Button.TabIndex = 1;
+            this._01Button.UseVisualStyleBackColor = true;
+            this._01Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // _02Button
+            // 
+            this._02Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._02Button.Location = new System.Drawing.Point(197, 12);
+            this._02Button.Name = "_02Button";
+            this._02Button.Size = new System.Drawing.Size(75, 75);
+            this._02Button.TabIndex = 2;
+            this._02Button.UseVisualStyleBackColor = true;
+            this._02Button.Click += new System.EventHandler(this._02Button_Click);
+            // 
+            // _10Button
+            // 
+            this._10Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._10Button.Location = new System.Drawing.Point(12, 93);
+            this._10Button.Name = "_10Button";
+            this._10Button.Size = new System.Drawing.Size(75, 75);
+            this._10Button.TabIndex = 3;
+            this._10Button.UseVisualStyleBackColor = true;
+            this._10Button.Click += new System.EventHandler(this._10Button_Click);
+            // 
+            // _11Button
+            // 
+            this._11Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._11Button.Location = new System.Drawing.Point(104, 93);
+            this._11Button.Name = "_11Button";
+            this._11Button.Size = new System.Drawing.Size(75, 75);
+            this._11Button.TabIndex = 4;
+            this._11Button.UseVisualStyleBackColor = true;
+            this._11Button.Click += new System.EventHandler(this._11Button_Click);
+            // 
+            // _12Button
+            // 
+            this._12Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._12Button.Location = new System.Drawing.Point(197, 93);
+            this._12Button.Name = "_12Button";
+            this._12Button.Size = new System.Drawing.Size(75, 75);
+            this._12Button.TabIndex = 5;
+            this._12Button.UseVisualStyleBackColor = true;
+            this._12Button.Click += new System.EventHandler(this._12Button_Click);
+            // 
+            // _20Button
+            // 
+            this._20Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._20Button.Location = new System.Drawing.Point(12, 174);
+            this._20Button.Name = "_20Button";
+            this._20Button.Size = new System.Drawing.Size(75, 75);
+            this._20Button.TabIndex = 6;
+            this._20Button.UseVisualStyleBackColor = true;
+            this._20Button.Click += new System.EventHandler(this._20Button_Click);
+            // 
+            // _21Button
+            // 
+            this._21Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._21Button.Location = new System.Drawing.Point(104, 174);
+            this._21Button.Name = "_21Button";
+            this._21Button.Size = new System.Drawing.Size(75, 75);
+            this._21Button.TabIndex = 7;
+            this._21Button.UseVisualStyleBackColor = true;
+            this._21Button.Click += new System.EventHandler(this._21Button_Click);
+            // 
+            // _22Button
+            // 
+            this._22Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._22Button.Location = new System.Drawing.Point(197, 174);
+            this._22Button.Name = "_22Button";
+            this._22Button.Size = new System.Drawing.Size(75, 75);
+            this._22Button.TabIndex = 8;
+            this._22Button.UseVisualStyleBackColor = true;
+            this._22Button.Click += new System.EventHandler(this._22Button_Click);
+            // 
+            // _turnLabel
+            // 
+            this._turnLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this._turnLabel.Location = new System.Drawing.Point(0, 298);
+            this._turnLabel.Name = "_turnLabel";
+            this._turnLabel.Size = new System.Drawing.Size(284, 13);
+            this._turnLabel.TabIndex = 9;
+            this._turnLabel.Text = "label1";
+            // 
+            // XOForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::XOGame.Properties.Resources.smallSpace1;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this._turnLabel);
+            this.Controls.Add(this._22Button);
+            this.Controls.Add(this._21Button);
+            this.Controls.Add(this._20Button);
+            this.Controls.Add(this._12Button);
+            this.Controls.Add(this._11Button);
+            this.Controls.Add(this._10Button);
+            this.Controls.Add(this._02Button);
+            this.Controls.Add(this._01Button);
+            this.Controls.Add(this._00Button);
+            this.Name = "XOForm";
+            this.Text = "XO Wars";
+            this.Load += new System.EventHandler(this.XOForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _00Button;
+        private System.Windows.Forms.Button _01Button;
+        private System.Windows.Forms.Button _02Button;
+        private System.Windows.Forms.Button _10Button;
+        private System.Windows.Forms.Button _11Button;
+        private System.Windows.Forms.Button _12Button;
+        private System.Windows.Forms.Button _20Button;
+        private System.Windows.Forms.Button _21Button;
+        private System.Windows.Forms.Button _22Button;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label _turnLabel;
+    }
+}
+
---
+++ XOGame/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOGame
+{
+    public partial class XOForm : Form
+    {
+        private Turn _turn;
+        private char[,] grid = new char [3,3];
+
+        private void OnFlipped(object sender, EventArgs e)
+        {
+            _turnLabel.Text = string.Format("{0} Go!", _turn.WhoseTurn);
+        }
+
+        public XOForm()
+        {
+            InitializeComponent();
+        }
+
+        private void XOForm_Load(object sender, EventArgs e)
+        {
+            _turn = new Turn();
+            _turn.Flipped += OnFlipped;
+            OnFlipped(null, EventArgs.Empty);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _01Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[0, 1] = _turn.WhoseTurn;
+            _turn.Flip();
+            _01Button.Enabled = false;
+            check();
+        }
+
+        private void _00Button_Click(object sender, EventArgs e)
+        {
+            _00Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[0,0] = _turn.WhoseTurn;
+            _turn.Flip();
+            _00Button.Enabled = false;
+            check();
+
+        }
+
+        private void _02Button_Click(object sender, EventArgs e)
+        {
+            _02Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[0, 2] = _turn.WhoseTurn;
+            _turn.Flip();
+            _02Button.Enabled = false;
+            check();
+        }
+
+        private void _10Button_Click(object sender, EventArgs e)
+        {
+            _10Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[1, 0] = _turn.WhoseTurn;
+            _turn.Flip();
+            _10Button.Enabled = false;
+            check();
+        }
+
+        private void _11Button_Click(object sender, EventArgs e)
+        {
+            _11Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[1, 1] = _turn.WhoseTurn;
+            _turn.Flip();
+            _11Button.Enabled = false;
+            check();
+        }
+
+        private void _12Button_Click(object sender, EventArgs e)
+        {
+            _12Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[1, 2] = _turn.WhoseTurn;
+            _turn.Flip();
+            _12Button.Enabled = false;
+            check();
+        }
+
+        private void _20Button_Click(object sender, EventArgs e)
+        {
+            _20Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[2, 0] = _turn.WhoseTurn;
+            _turn.Flip();
+            _20Button.Enabled = false;
+            check();
+        }
+
+        private void _21Button_Click(object sender, EventArgs e)
+        {
+            _21Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[2, 1] = _turn.WhoseTurn;
+            _turn.Flip();
+            _21Button.Enabled = false;
+            check();
+        }
+
+        private void _22Button_Click(object sender, EventArgs e)
+        {
+            _22Button.Text = string.Format("{0}", _turn.WhoseTurn);
+            grid[2, 2] = _turn.WhoseTurn;
+            _turn.Flip();
+            _22Button.Enabled = false;
+            check();
+        }
+
+        void check()
+        {
+            /*  x
+             *      x
+             *          x
+             */
+            if (_00Button.Text == "X" && _11Button.Text == "X" && _22Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*  o
+             *      o
+             *          o
+             */
+            if (_00Button.Text == "O" && _11Button.Text == "O" && _22Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+            /*          x
+             *      x
+             *  x
+             */
+            if (_02Button.Text == "X" && _11Button.Text == "X" && _20Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*          o
+             *      o
+             *  o
+             */
+            if (_02Button.Text == "O" && _11Button.Text == "O" && _20Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+            /*  x   x   x
+             * 
+             */
+            if (_00Button.Text == "X" && _01Button.Text == "X" && _02Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*  o   o   o
+             * 
+             */
+            if (_00Button.Text == "O" && _01Button.Text == "O" && _02Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+            /*  x
+             *  x
+             *  x
+             */
+            if (_00Button.Text == "X" && _10Button.Text == "X" && _20Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*  o
+             *  o
+             *  o
+             */
+            if (_00Button.Text == "O" && _10Button.Text == "O" && _20Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+            /*          x
+             *          x
+             *          x
+             */
+            if (_02Button.Text == "X" && _12Button.Text == "X" && _22Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*          o
+             *          o
+             *          o
+             */
+            if (_02Button.Text == "O" && _12Button.Text == "O" && _22Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+            /*      x
+             *      x
+             *      x
+             */
+            if (_01Button.Text == "X" && _11Button.Text == "X" && _21Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*      o
+             *      o
+             *      o
+             */
+            if (_01Button.Text == "O" && _11Button.Text == "O" && _21Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+            /*
+             * 
+             *  x   x   x
+             */
+            if (_20Button.Text == "X" && _21Button.Text == "X" && _22Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*
+             * 
+             *  o   o   o
+             */
+            if (_20Button.Text == "O" && _21Button.Text == "O" && _22Button.Text == "O")
+            {
+                MessageBox.Show("Player 0 won.");
+            }
+            /*
+             *  x   x   x 
+             *
+             */
+            if (_10Button.Text == "X" && _11Button.Text == "X" && _12Button.Text == "X")
+            {
+                MessageBox.Show("Player X won.");
+            }
+            /*
+             *  o   o   o 
+             *
+             */
+            if (_10Button.Text == "O" && _11Button.Text == "O" && _12Button.Text == "O")
+            {
+                MessageBox.Show("Player O won.");
+            }
+
+            //if none of the above if statements are caught and no buttons are left as empty text, display a tie
+            if(_00Button.Text != "" && _01Button.Text != "" && _02Button.Text != "" &&
+                _10Button.Text != "" && _11Button.Text != "" && _12Button.Text != "" &&
+                _20Button.Text != "" && _21Button.Text != "" && _22Button.Text != "")
+            {
+                MessageBox.Show("Tie.");
+            }
+        }
+
+
+    }
+
+    public class Turn
+        {
+            private char _whoseTurn = 'X';
+            private int _turnsLeft = 9;
+
+            public char WhoseTurn
+            {
+                get { return _whoseTurn; }
+            }
+
+            public int TurnsLeft
+            {
+                get { return _turnsLeft; }
+            }
+
+            public void Flip()
+            {
+                _whoseTurn = (_whoseTurn == 'X') ? 'O' : 'X';
+                _turnsLeft--;
+                if (_turnsLeft < 0)
+                    throw new ApplicationException("TurnsLeft cannot go lower than 0");
+                OnFlipped();
+            }
+
+            public event EventHandler Flipped;
+
+            private void OnFlipped()
+            {
+                EventHandler flipped = Flipped;
+                if (flipped != null)
+                {
+                    flipped(this, EventArgs.Empty);
+                }
+            }
+        }
+}
